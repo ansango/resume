bce2aa5b88a9fba8af209f090b09712f0dfb19db
anibalsantos <anibalsantosgo@gmail.com>
2020-12-20 16:53:37 +0100

    ga

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,57 +1,54 @@
 <template>
-  <!-- component -->
-  <div class="w-full text-gray-600 bg-white dark:bg-gray-900">
-    <div class="p-4">
-      <nav class="flex-row flex justify-end">
-        <nuxt-link
-          v-for="locale in availableLocales"
-          :key="locale.code"
-          :to="switchLocalePath(locale.code)"
-          class="px-4 py-3 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 md:ml-4 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-          @click="reload"
-          >{{ `${locale.code}` }}</nuxt-link
+  <div class="">
+    <nav
+      class="w-full flex justify-end text-gray-600 bg-white dark:bg-gray-900 p-2"
+    >
+      <nuxt-link
+        v-for="locale in availableLocales"
+        :key="locale.code"
+        :to="switchLocalePath(locale.code)"
+        class="px-4 py-3 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 md:ml-4 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+        @click="reload"
+        >{{ `${locale.code}` }}</nuxt-link
+      >
+      <button
+        class="flex flex-row items-center px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+        @click="toggle"
+      >
+        <svg
+          v-if="$colorMode.preference === 'light'"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-        <div class="relative">
-          <button
-            class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-            @click="toggle"
-          >
-            <svg
-              v-if="$colorMode.preference === 'light'"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </svg>
-            <svg
-              v-if="$colorMode.preference === 'dark'"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </nav>
-    </div>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+        <svg
+          v-if="$colorMode.preference === 'dark'"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+      </button>
+    </nav>
   </div>
 </template>
 
@@ -67,6 +64,12 @@ export default {
     toggle() {
       this.$colorMode.preference =
         this.$colorMode.preference === 'light' ? 'dark' : 'light'
+      this.$ga.event({
+        eventCategory: 'language',
+        eventAction: 'toggle',
+        eventLabel: 'switch-language',
+        eventValue: 1,
+      })
     },
     reload() {
       location.reload()
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'this is my cv, check my skills and hire me for build your next awesome project!',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -17,13 +22,55 @@ export default {
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
+    '@nuxtjs/html-validator',
+    '@nuxtjs/google-analytics',
   ],
 
-  purgeCSS: {
-    whitelist: ['dark-mode'],
+  htmlValidator: {
+    usePrettier: false,
+    options: {
+      extends: [
+        'html-validate:document',
+        'html-validate:recommended',
+        'html-validate:standard',
+      ],
+      rules: {
+        'svg-focusable': 'off',
+        'no-unknown-elements': 'error',
+        // Conflicts or not needed when using prettier formatting
+        'void-style': 'off',
+        'no-trailing-whitespace': 'off',
+        // Conflict with Nuxt defaults
+        'require-sri': 'off',
+        'attribute-boolean-style': 'off',
+        // Unreasonable rule
+        'no-inline-style': 'off',
+      },
+    },
   },
-  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', 'nuxt-i18n'],
+
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', 'nuxt-i18n', 'nuxt-precompress'],
   axios: {},
+  googleAnalytics: {
+    id: 'UA-185847456-1',
+    checkDuplicatedScript: true,
+    dev: true,
+    autoTracking: {
+      screenview: true,
+      pageviewTemplate(route) {
+        return {
+          page: route.path,
+          title: document.title,
+          location: window.location.href,
+        }
+      },
+    },
+    skipSamePath: true,
+    debug: {
+      enabled: true,
+      sendHitTask: true,
+    },
+  },
   i18n: {
     vueI18nLoader: true,
     detectBrowserLanguage: {
@@ -45,6 +92,45 @@ export default {
       },
     ],
     vueI18n: i18n,
+  },
+  purgeCSS: {
+    whitelist: ['dark-mode'],
+  },
+  nuxtPrecompress: {
+    enabled: true, // Enable in production
+    report: false, // set true to turn one console messages during module init
+    test: /\.(js|css|html|txt|xml|svg)$/, // files to compress on build
+    // Serving options
+    middleware: {
+      // You can disable middleware if you serve static files using nginx...
+      enabled: true,
+      // Enable if you have .gz or .br files in /static/ folder
+      enabledStatic: true,
+      // Priority of content-encodings, first matched with request Accept-Encoding will me served
+      encodingsPriority: ['br', 'gzip'],
+    },
+
+    // build time compression settings
+    gzip: {
+      // should compress to gzip?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].gz[query]', // middleware will look for this filename
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: { level: 9 },
+    },
+    brotli: {
+      // should compress to brotli?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].br[query]', // middleware will look for this filename
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8,
+    },
   },
   build: {},
 }
